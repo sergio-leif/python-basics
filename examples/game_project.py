d7f5dd4108609ac5ab7864efa7820dc6c12ebb3d
Sergio <sergio.leiva@sngular.com>
2021-07-25 15:59:35 -0400

    Creation class

--- examples/game_project.py
+++ examples/game_project.py
@@ -1,4 +1,16 @@
 
 class PlayerCharacter:
-    def __init__(self, name):
+    def __init__(self, name, age):
         self.name = name
+        self.age = age
+
+    def run(self):
+      print('run')
+      return 'done'
+
+player1 = PlayerCharacter("Sergio", 28)
+player2 = PlayerCharacter("Tommy", 32)
+
+print(player1.name)
+print(player1.run())
+print(player2.age)
