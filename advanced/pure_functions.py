a7082f9d484d5ce34a075fd74854a186a0e6d324
Sergio <sergio.leiva@sngular.com>
2021-08-23 12:11:19 -0400

    map, zip, filter and reduce

--- advanced/pure_functions.py
+++ advanced/pure_functions.py
@@ -1,5 +1,7 @@
 # map, filter, zip, and reduce
 
+from functools import reduce
+
 my_list = [1,2,3]
 
 # Map
@@ -7,11 +9,18 @@ def multiply_by2(item):
     return item*2
 
 # print(multiply_by2([1,2,3]))
-print(list(map(multiply_by2, my_list)))
+# print(list(map(multiply_by2, my_list)))
 
 
 # Filter
 def only_odd(item):
     return item % 2 != 0
 
-print(list(filter(only_odd, my_list)))
+# print(list(filter(only_odd, my_list)))
+
+def accumulator(acc, item):
+    print(acc, item)
+    return acc + item
+
+
+print(reduce(accumulator, my_list, 0))
