f09bacbaf12a6c44f7b33fb0799d736d4ca611d5
Sergio <sergio.leiva@sngular.com>
2021-09-15 12:32:03 -0400

    Translator exercise

---
+++ advanced/exercises/translate_file.py
@@ -0,0 +1,15 @@
+from translate import Translator
+
+translator= Translator(to_lang="es") # zh: chinese
+
+try:
+    with open('./file_to_translate.txt', mode='r') as my_file:
+        text = my_file.readlines()
+        for line in text:
+            print(translator.translate(line))
+except FileNotFoundError as err:
+    print('File does not exist')
+    raise err
+except IOError as err:
+    print('IO error')
+    raise err
